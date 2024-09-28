eda7f515ea4f3617c3b446ada334a6c2b26e0639
filip <filip@FILIPDAB-PC>
2024-09-28 13:03:38 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Tester płci!");
+Console.WriteLine("Jesteś mężczyzną wpisz [m] jeśli kobietą [k]");
+string plec = Console.ReadLine();
+if (plec == "m")
+    Console.WriteLine("Jesteś facetem!");
+else Console.WriteLine("Jesteś babą!");
